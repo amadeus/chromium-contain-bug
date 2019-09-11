9bb0074d2dc41a4c29f5b8b568435972698cef99
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2019-09-10 23:42:22 -0700

    Chrome bug

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
+  const [contain, setContain] = useState("paint");
+  const handleClick = () => setContain(contain === "paint" ? "none" : "paint");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container} style={{ contain: contain }}>
+      <button onClick={handleClick}>Toggle Paint</button>
+      <div className={styles.transformed} />
+      <div className={styles.content}>
+        Lorem Ipsum has been the industry's standard dummy text ever since the
+        1500s, when an unknown printer took a galley of type and scrambled it to
+        make a type specimen book. It has survived not only five centuries, but
+        also the leap into electronic typesetting, remaining essentially
+        unchanged. It was popularised in the 1960s with the release of Letraset
+        sheets containing Lorem Ipsum passages, and more recently with desktop
+        publishing software like Aldus PageMaker including versions of Lorem
+        Ipsum.
+      </div>
     </div>
   );
 }
