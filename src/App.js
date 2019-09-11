99d243ac74ca3ef901aa2feb00758bdb723b7656
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2019-09-10 23:58:33 -0700

    Bug report prep

--- src/App.js
+++ src/App.js
@@ -5,20 +5,27 @@ function App() {
   const [contain, setContain] = useState("paint");
   const handleClick = () => setContain(contain === "paint" ? "none" : "paint");
   return (
-    <div className={styles.container} style={{ contain: contain }}>
-      <button onClick={handleClick}>Toggle Paint</button>
-      <div className={styles.transformed} />
-      <div className={styles.content}>
-        Lorem Ipsum has been the industry's standard dummy text ever since the
-        1500s, when an unknown printer took a galley of type and scrambled it to
-        make a type specimen book. It has survived not only five centuries, but
-        also the leap into electronic typesetting, remaining essentially
-        unchanged. It was popularised in the 1960s with the release of Letraset
-        sheets containing Lorem Ipsum passages, and more recently with desktop
-        publishing software like Aldus PageMaker including versions of Lorem
-        Ipsum.
+    <>
+      <div className={styles.container} style={{ contain: contain }}>
+        <button onClick={handleClick}>Toggle Container Paint</button>
+        <div className={styles.transformed}>
+          The element has `transform: translate3d(0,0,0)` on it
+        </div>
+        <div>
+          Look closely at this text on Windows, on a monitor that is set to the
+          default 100% zoom, while clicking Toggle Paint. When contain: paint is
+          set on the container, the 3d transform affects all text rendering in
+          the container
+        </div>
       </div>
-    </div>
+      <a
+        href="https://github.com/amadeus/chromium-contain-bug"
+        // eslint-disable-next-line
+        target="_blank"
+      >
+        Source Code
+      </a>
+    </>
   );
 }
 
